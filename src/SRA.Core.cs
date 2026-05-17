04db070ca74664fb779896ffa50d97317ccac847
RealMG <realmg51@gmail.com>
2026-05-17 09:12:06 +0700

    feat: add logic code

---
+++ src/SRA.Core.cs
@@ -0,0 +1,18 @@
+using System;
+namespace SRA
+{
+	class Core
+	{
+		static void Logic
+		{
+			bool run = true;
+			while(run)
+			{
+				// for null safety
+				string input = Console.ReadLine()?.ToLower?.Trim() ?? "";
+				// logic, now i don't have times
+				// todo: add logic below:
+			}
+		}
+	}
+}
