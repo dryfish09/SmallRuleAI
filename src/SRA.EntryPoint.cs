cb402477095d7ce252fb46f343ad534659f14fa4
RealMG <realmg51@gmail.com>
2026-05-17 09:20:26 +0700

    format: format code

--- src/SRA.EntryPoint.cs
+++ src/SRA.EntryPoint.cs
@@ -7,14 +7,14 @@ namespace SRA
     {
       Console.WriteLine("Welcome! Type 'exit' to exit program");
       try
-      {
+	  {
 				Core.Logic();
-			}
-			catch (Expection e)
-			{
+	  }
+		catch (Expection e)
+		{
 				Console.WriteLine($"Unexpected error: {e}");
-			}
 		}
 	}
+  }
 }
         
