35fdee53661c818c12d03525ca83d57784ad4e80
RealMG <realmg51@gmail.com>
2026-05-17 12:42:55 +0700

    feat: add response handling

---
+++ src/SRA.InputHandle.cs
@@ -0,0 +1,15 @@
+using System;
+namespace SRA
+{
+	class Response
+	{
+		static void HandleNull()
+		{
+			Console.WriteLine("Oops! Please talk anything!");
+		}
+		static void Greet()
+		{
+			Console.WriteLine("Hi there! What's up!");
+		}
+	}
+}
