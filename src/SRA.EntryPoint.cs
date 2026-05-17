1fffd19082ee765e87af6abe83641783ff0f84a6
RealMG <realmg51@gmail.com>
2026-05-17 09:16:53 +0700

    feat: add entry point

---
+++ src/SRA.EntryPoint.cs
@@ -0,0 +1,20 @@
+using System;
+namespace SRA
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Welcome! Type 'exit' to exit program");
+      try
+      {
+				Core.Logic();
+			}
+			catch (Expection e)
+			{
+				Console.WriteLine($"Unexpected error: {e}");
+			}
+		}
+	}
+}
+        
