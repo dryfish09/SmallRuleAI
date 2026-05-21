5f48fe7f81a80c65213df593711b8e13953012bd
RealMG <realmg51@gmail.com>
2026-05-21 20:12:00 +0700

    feat: add random welcome

--- src/SRA.EntryPoint.cs
+++ src/SRA.EntryPoint.cs
@@ -1,19 +1,27 @@
 using System;
+
 namespace SRA
 {
-  class Program
-  {
-    static void Main()
+    class Program
     {
-      Console.WriteLine("Welcome! Type 'exit' to exit program");
-      try
-      {
-        Core.Logic();
-      }
-      catch (Exception e) 
-      {
-        Console.WriteLine($"Unexpected error: {e}");
-      }
+        static void Main()
+        {
+            Random rand = new Random();
+            string[] Starts = {
+                "Hello! Let's start a new happy day by chatting with me!",
+                "How can I help you?",
+                "Ready to start?"
+            };
+            string start = Starts[rand.Next(Starts.Length)];
+            Console.WriteLine(start);
+            try
+            {
+                Core.Logic();
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine($"Unexpected error: {e}");
+            }
+        }
     }
-  }
 }
