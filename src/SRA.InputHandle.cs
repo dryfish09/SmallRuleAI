94ef177c8f296c542ee20d0d9a712c0403205b72
RealMG <realmg51@gmail.com>
2026-05-21 21:50:02 +0700

    feat: add random responses

--- src/SRA.InputHandle.cs
+++ src/SRA.InputHandle.cs
@@ -1,27 +1,69 @@
 using System;
+
 namespace SRA
 {
-	class Response
-	{
-		public static void HandleNull()
-		{
-			Console.WriteLine("Oops! Please talk anything!");
-		}
-		public static void Greet()
-		{
-			Console.WriteLine("Hi there! What's up!");
-		}
-		public static void SayFine()
-		{
-			Console.WriteLine("Fine, thank you!");
-		}
-		public static void SayBye()
-		{
-			Console.WriteLine("Bye! See you again!");
-		}
-		public static void HandleElse()
-		{
-			Console.WriteLine("Sorry, I don't know about that");
-		}
-	}
+    class Response
+    {
+        private static Random rand = new Random();
+        
+        private static string[] nullResponses = {
+            "Oops! Please talk anything!",
+            "You didn't say anything! Try again!",
+            "Hello? Are you there?",
+            "Don't be shy, say something!"
+        };
+        
+        private static string[] greetResponses = {
+            "Hi there! What's up!",
+            "Hello! Nice to see you!",
+            "Hey! How's your day going?",
+            "Greetings! What can I do for you?"
+        };
+        
+        private static string[] fineResponses = {
+            "Fine, thank you!",
+            "I'm doing great! Thanks for asking!",
+            "Wonderful! And you?",
+            "Couldn't be better!"
+        };
+        
+        private static string[] byeResponses = {
+            "Bye! See you again!",
+            "Goodbye! Have a nice day!",
+            "See you later! Take care!",
+            "Bye bye! Come back soon!"
+        };
+        
+        private static string[] elseResponses = {
+            "Sorry, I don't know about that",
+            "I'm not sure how to respond to that",
+            "Interesting! Tell me more!",
+            "Hmm, I haven't learned about that yet"
+        };
+        
+        public static void HandleNull()
+        {
+            Console.WriteLine(nullResponses[rand.Next(nullResponses.Length)]);
+        }
+        
+        public static void Greet()
+        {
+            Console.WriteLine(greetResponses[rand.Next(greetResponses.Length)]);
+        }
+        
+        public static void SayFine()
+        {
+            Console.WriteLine(fineResponses[rand.Next(fineResponses.Length)]);
+        }
+        
+        public static void SayBye()
+        {
+            Console.WriteLine(byeResponses[rand.Next(byeResponses.Length)]);
+        }
+        
+        public static void HandleElse()
+        {
+            Console.WriteLine(elseResponses[rand.Next(elseResponses.Length)]);
+        }
+    }
 }
